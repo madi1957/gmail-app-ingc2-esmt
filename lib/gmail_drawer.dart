b7de103acf6e968a1e7fe15ecb15d85d33e71a75
madi1957 <ymeria1957@gmail.com>
2025-12-11 11:29:17 +0000

    first commit

--- lib/gmail_drawer.dart
+++ lib/gmail_drawer.dart
@@ -1,29 +1,37 @@
 import 'package:flutter/material.dart';
 
+// Widget représentant le Drawer façon Gmail
 class GmailDrawer extends StatelessWidget {
   const GmailDrawer({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Drawer(
-      child: ListView(
-        children: [
-          SizedBox(
-                height: 80,
-                child: DrawerHeader(
-                decoration: BoxDecoration(
-                  
-                ),
-          child : Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
+      child: Container(
+        // Le container permet d'ajouter un fond ou un style ou ce qu'on veut sur le design
+        child: ListView(
+          // ListView permet de scroller dans le Drawer
+          children: [
+            // En-tête du Drawer (logo,menu,titre)
+            SizedBox(
+              height: 80, // hauteur du header
+              child: DrawerHeader(
+                // Le DrawerHeader c'est l'entête du Drawer
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    // Logo Gmail 
                     Image.asset("assets/images/logo_gmail.jpg"),
+
+                    // Icône menu permettant de fermer le Drawer
                     IconButton(
-                    icon: Icon(Icons.menu, color: Colors.black),
-                    onPressed: () {
-                      Navigator.pop(context); // ferme le drawer
-                    },
-                  ),
+                      icon: Icon(Icons.menu, color: Colors.black),
+                      onPressed: () {
+                        Navigator.pop(context); // ferme le Drawer
+                      },
+                    ),
+
+                    // Texte "Gmail" stylisé
                     Text(
                       'Gmail',
                       style: TextStyle(
@@ -34,57 +42,124 @@ class GmailDrawer extends StatelessWidget {
                     ),
                   ],
                 ),
+              ),
+            ),
+
+            // Bouton "Nouveau message" stylisé
+            Container(
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(30), // coins arrondis
+              ),
+              child: ListTile(
+                leading: const Icon(Icons.edit, color: Colors.grey),
+                title: const Text("Nouveau message"),
+                onTap: () {
+                  // Action pour écrire un nouveau mail
+                },
+              ),
+            ),
+
+            // Boîte de réception et le nombre de mails non lus
+            ListTile(
+              leading: const Icon(Icons.inbox, color: Colors.grey),
+              trailing: Container(
+                padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 4),
+                decoration: BoxDecoration(
+                  color: Colors.pink,
+                  borderRadius: BorderRadius.circular(12),
                 ),
-          ),
-          Container(
-            decoration: BoxDecoration(
-              color: Colors.white,
-             borderRadius: BorderRadius.circular(30),
+                child: const Text(
+                  "1190",
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+              title: const Text("Boîte de réception"),
             ),
-            child: ListTile(
-              leading: const Icon(Icons.edit, color: Colors.grey),
-              title: const Text("Nouveau message"),
-              onTap: () {
-                // Action à définir pour la création d'un nouveau message
-              },
+
+            // Messages suivis 
+
+            ListTile(
+              leading: const Icon(Icons.star_border, color: Colors.grey),
+              title: const Text("Messages suivis"),
             ),
-          ),
-            
-          ListTile(
-            leading: const Icon(Icons.inbox, color: Colors.grey),
-            title: const Text("Boîte de réception"),
-          ),
+            // En attente
+            ListTile(
+              leading: const Icon(Icons.schedule, color: Colors.grey),
+              title: const Text("En attente"),
+            ),
+            // Messages envoyés
+            ListTile(
+              leading: const Icon(Icons.send, color: Colors.grey),
+              title: const Text("Messages envoyés"),
+            ),
 
-          ListTile(
-            leading: const Icon(Icons.star_border, color: Colors.grey),
-            title: const Text("Messages suivis"),
-          ),
+            // Brouillons avec badge vert
+            ListTile(
+              leading: const Icon(Icons.inbox, color: Colors.grey),
+              trailing: Container(
+                //pour les espacement au niveau du box de message non lu
+                padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 4),
+                decoration: BoxDecoration(
+                  color: Colors.green,
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                child: const Text(
+                  "12",
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+              title: const Text("Brouillons"),
+            ),
 
-          ListTile(
-            leading: const Icon(Icons.schedule, color: Colors.grey),
-            title: const Text("En attente"),
-          ),
+            ListTile(
+              leading: const Icon(Icons.shopping_cart, color: Colors.grey),
+              title: const Text("Achats"),
+            ),
 
-          ListTile(
-            leading: const Icon(Icons.send, color: Colors.grey),
-            title: const Text("Messages envoyés"),
-          ),
+            // Important
+            ListTile(
+              leading: Icon(Icons.label_important, color: Colors.grey),
+              title: Text("Important"),
+            ),
 
-          ListTile(
-            leading: const Icon(Icons.drafts, color: Colors.grey),
-            title: const Text("Brouillons"),
-          ),
+            // Planifié
+            ListTile(
+              leading: Icon(Icons.schedule, color: Colors.grey),
+              title: Text("Planifié"),
+            ),
 
-          ListTile(
-            leading: const Icon(Icons.shopping_cart, color: Colors.grey),
-            title: const Text("Achats"),
-          ),
+            // Tous les messages
+            ListTile(
+              leading: Icon(Icons.all_inbox, color: Colors.grey),
+              title: Text("Tous les messages"),
+            ),
 
-          ListTile(
-            leading: const Icon(Icons.delete_outline, color: Colors.grey),
-            title: const Text("Corbeille"),
-          ),
-        ],
+            // Spam
+            ListTile(
+              leading: Icon(Icons.report_gmailerrorred, color: Colors.grey),
+              title: Text("Spam"),
+            ),
+
+            // Corbeille
+            ListTile(
+              leading: const Icon(Icons.delete_outline, color: Colors.grey),
+              title: const Text("Corbeille"),
+            ),
+
+            // Gérer les abonnements
+            ListTile(
+              leading: Icon(Icons.subscriptions, color: Colors.grey),
+              title: Text("Gérer les abonnements"),
+            ),
+
+            // Corbeille 
+            ListTile(
+              leading: Icon(Icons.label, color: Colors.grey),
+              title: Text("Gérer les libellés"),
+            ),
+          ],
+        ),
       ),
     );
   }
